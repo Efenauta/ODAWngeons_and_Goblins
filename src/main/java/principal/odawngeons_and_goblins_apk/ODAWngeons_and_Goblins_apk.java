1123b4a139c4fe927603cec9113339488730d0e8
ivane <ivane@Sobremesa-Efenauta>
2024-02-11 14:24:59 +0100

    Carga de archivos

---
+++ src/main/java/principal/odawngeons_and_goblins_apk/ODAWngeons_and_Goblins_apk.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal.odawngeons_and_goblins_apk;
+
+/**
+ *
+ * @author ivane
+ */
+public class ODAWngeons_and_Goblins_apk {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
