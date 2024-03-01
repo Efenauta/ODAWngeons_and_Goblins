7c375778a3d136b6b5ccea1a66891eb7a5374bde
ivane <ivane@Sobremesa-Efenauta>
2024-03-01 15:38:07 +0100

    subida main prueba superada (modified)

--- src/main/java/principal/odawngeons_and_goblins_apk/ODAWngeons_and_Goblins_apk.java
+++ src/main/java/principal/odawngeons_and_goblins_apk/ODAWngeons_and_Goblins_apk.java
@@ -4,6 +4,7 @@ package principal.odawngeons_and_goblins_apk;
 public class ODAWngeons_and_Goblins_apk {
 
     public static void main(String[] args) {
-
+        
+        System.out.println("Prueba solucionada!");
     }
 }
