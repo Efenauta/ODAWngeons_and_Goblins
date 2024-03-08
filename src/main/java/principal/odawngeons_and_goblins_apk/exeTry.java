64ed065f30fc0a79c0f58b58135a6d90e49eb32c
ivane <ivane@Sobremesa-Efenauta>
2024-03-08 03:25:26 +0100

    cambios en las funciones de menus

--- src/main/java/principal/odawngeons_and_goblins_apk/exeTry.java
+++ src/main/java/principal/odawngeons_and_goblins_apk/exeTry.java
@@ -3,7 +3,7 @@ package principal.odawngeons_and_goblins_apk;
 public class exeTry {
 
     public static void main(String[] args) {
-        ODAWngeons_and_Goblins_apk game = new ODAWngeons_and_Goblins_apk();
-        game.game();
+        ODAWngeons_and_Goblins_apk juego = new ODAWngeons_and_Goblins_apk();
+        juego.game();
     }
 }
